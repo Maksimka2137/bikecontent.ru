0f058b1a8738be43d9670bd340fc8125a6484712
Alex Balaboshko <alex.balaboshko@gmail.com>
2024-12-21 15:46:06 +0300

    Add new default template layout and styles
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,74 +1,81 @@
 import fs from 'node:fs'
 
 const locales = fs.readdirSync('locales')
-  .map(file => ({
+.map(file => ({
     code: file.replace(/\.(yml|yaml|json)$/, ''),
     file,
-  }))
+}))
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: 'Nightrunner',
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    app: {
+        head: {
+            title: 'Nightrunner',
+            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+            script: [
+                {'src': '/js/main.js', tagPosition: 'bodyClose'},
+                {'src': '/js/plugins.js', tagPosition: 'bodyClose'}
+            ],
+        },
     },
-  },
-  imports: { // add folders here to auto-import them in your application
-    dirs: [
-      'stores',
-      'stores/**',
-      'composables/**',
+    imports: { // add folders here to auto-import them in your application
+        dirs: [
+            'stores',
+            'stores/**',
+            'composables/**',
+        ],
+    },
+    components: [{ path: '~/components', pathPrefix: false }],
+    typescript: {
+        tsConfig: {
+            compilerOptions: {
+                moduleResolution: 'bundler',
+            },
+        },
+    },
+    vite: {
+        vue: {
+            script: {
+                defineModel: true,
+                propsDestructure: true,
+            },
+        },
+    },
+    experimental: {
+        typedPages: true,
+    },
+
+    // uncomment to disable SSR. This will basically make the app a SPA, like a normal Vue app, but with all the Nuxt goodies
+    // ssr: false,
+
+    // global CSS files
+    css: [
+        // '@unocss/reset/tailwind.css',
+        // '@/assets/css/reset.css',
+        // '@/assets/css/main.css',
+        '@/assets/css/import/vendor.css',
+        '@/assets/css/import/styles.css',
     ],
-  },
-  components: [{ path: '~/components', pathPrefix: false }],
-  typescript: {
-    tsConfig: {
-      compilerOptions: {
-        moduleResolution: 'bundler',
-      },
+
+    // plugin configurations
+    modules: [
+        '@nuxtjs/i18n',
+        '@vueuse/nuxt',
+        '@unocss/nuxt',
+        '@nuxtjs/critters',
+        '@nuxtjs/color-mode',
+        '@pinia/nuxt',
+    ],
+    i18n: {
+        langDir: 'locales',
+        defaultLocale: 'ru',
+        locales,
     },
-  },
-  vite: {
-    vue: {
-      script: {
-        defineModel: true,
-        propsDestructure: true,
-      },
+    colorMode: {
+        preference: 'system',
+        fallback: 'light',
+        classPrefix: '',
+        classSuffix: '',
+        storageKey: 'color-scheme',
     },
-  },
-  experimental: {
-    typedPages: true,
-  },
-
-  // uncomment to disable SSR. This will basically make the app a SPA, like a normal Vue app, but with all the Nuxt goodies
-  // ssr: false,
-
-  // global CSS files
-  css: [
-    '@unocss/reset/tailwind.css',
-    '@/assets/css/reset.css',
-  ],
-
-  // plugin configurations
-  modules: [
-    '@nuxtjs/i18n',
-    '@vueuse/nuxt',
-    '@unocss/nuxt',
-    '@nuxtjs/critters',
-    '@nuxtjs/color-mode',
-    '@pinia/nuxt',
-  ],
-  i18n: {
-    langDir: 'locales',
-    defaultLocale: 'ru',
-    locales,
-  },
-  colorMode: {
-    preference: 'system',
-    fallback: 'light',
-    classPrefix: '',
-    classSuffix: '',
-    storageKey: 'color-scheme',
-  },
 })
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,5 @@
 <template>
-  <h1 class="title">
-    {{ t('main.title') }}
-  </h1>
-  <span class="flex-center flex-row gap-5">
-    <NuxtLinkLocale to="/about" class="btn">
-      {{ t('global.about') }}
-    </NuxtLinkLocale>
-    <NuxtLinkLocale to="/sample" class="btn">
-      {{ t('global.sample') }}
-    </NuxtLinkLocale>
-  </span>
+    <div></div>
 </template>
 
 
