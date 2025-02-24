01eb66b06c20cf6621ff9c309428df6eefc43158
Alex Balaboshko <alex.balaboshko@gmail.com>
2025-02-24 16:54:31 +0300

    Update deploy script
    Signed-off-by: Alex Balaboshko <alex.balaboshko@gmail.com>

--- bin/deploy_bike_to_producton.sh
+++ bin/deploy_bike_to_producton.sh
@@ -1,2 +1,7 @@
-pnpm generate && rsync -rva --delete .output/public/* user@alcocounter.ru:/home/user/www/bikecontent.ru/www
+#!/usr/bin/env bash
 
+cd "$(dirname "$0")/../frontend"
+pnpm generate
+rsync -rva --delete ./.output/public/* user@alcocounter.ru:/home/user/www/bikecontent.ru/www/frontend
+cd ../backend
+# rsync -rva --delete --exclude="__pycache__" --exclude=".venv" * user@alcocounter.ru:/home/user/www/bikecontent.ru/www/backend
